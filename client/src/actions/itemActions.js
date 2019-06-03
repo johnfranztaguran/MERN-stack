9cd3fa26bf4633a931b0c0a17c43395b469bdf38
franz <johnfranz.taguran@healthsplash.com>
2019-06-03 17:14:35 +0800

    basic backend, redux configuration, startup UI

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING } from './types';
+
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());
+  axios
+    .get('/api/items')
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    )
+};
+
+export const deleteItem = id => {
+  return {
+    type: DELETE_ITEMS,
+    payload: id
+  };
+};
+
+export const addItem = item => {
+  return {
+    type: ADD_ITEMS,
+    payload: item
+  };
+};
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING
+  };
+};
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://almighty_deku:imbalance143@ds131905.mlab.com:31905/mern_shopping'
+}
